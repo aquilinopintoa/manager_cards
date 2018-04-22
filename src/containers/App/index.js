189848d238e1f54bfe3e8d0934e4bba02490414d
Aquilino Pinto <aquilinopintoa@gmail.com>
2018-04-22 10:56:57 -0400

    componente para crear formulario
    Se incluyo validacion simple, despliegue desde modal en la app, integracion con state de app

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
-import ContentAdd from 'material-ui/svg-icons/content/add';
+import ContentAdd from 'material-ui/svg-icons/content/add'
+import Dialog from 'material-ui/Dialog';
 import FooterButton from '../../components/FooterButton'
+import FormCreateCard from  '../../components/FormCreateCard'
 import Header from '../../components/Header'
 import List from '../../components/List'
 
@@ -44,20 +46,49 @@ class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      items: testData
+      items: testData,
+      openCreateCard: false,
     }
+  }
+
+  handleOpen = () => {
+    this.setState({openCreateCard: true});
+  };
+
+  handleClose = () => {
+    this.setState({openCreateCard: false});
+  };
+
+  handleCreateCard = (card) => {
+    this.state.items.push(card)
+    this.setState({
+      items: this.state.items
+    })
+    this.handleClose()
   }
 
   render() {
     return (
       <div>
-        <Header/>
+        <Header
+          handleChange={this.handleChangeSorting}
+          sort={this.state.sort}/>
 
-        <List className="list" items={this.state.items}/>
-
-        <FooterButton>
+        <List items={this.state.items} sorting={this.state.sort}/>
+        
+        <FooterButton onClick={this.handleOpen}>
           <ContentAdd />
         </FooterButton>
+
+        <Dialog
+          contentClassName="contentModal"
+          modal={false}
+          open={this.state.openCreateCard}
+          onRequestClose={this.handleClose}
+          autoScrollBodyContent
+        >
+          <FormCreateCard onSubmit={this.handleCreateCard}/>
+        </Dialog>
       </div>
     )
   }
