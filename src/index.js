cecb4e2d9c1319f2aedafb23e1edf41cf7b8a323
Aquilino Pinto <aquilinopintoa@gmail.com>
2018-04-22 10:07:20 -0400

    flexboxgrid

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 
+import 'flexboxgrid/css/flexboxgrid.css'
 import './index.css'
 import App from './containers/App';
 
